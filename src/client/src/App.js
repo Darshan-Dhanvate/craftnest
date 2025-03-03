c2f529b6bca73959141f16c7fe860ce641a9c5fd
Darshan-Dhanvate <darshandhanvate55@gmail.com>
2025-03-03 22:59:31 +0530

    feat: Added new login page and UI updates

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,18 +1,24 @@
 import React, {useEffect, useState} from 'react';
 import {fetchData} from './services/api';
+import LoginSignUp from './components/Login-SignUp/LoginSignUp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
 
 const App= ()=>{
   const [data, setData]= useState(null);
 
-  useEffect(()=>{
-    fetchData().then(response => setData(response));
-  }, [])
+  // useEffect(()=>{
+  //   fetchData().then(response => setData(response));
+  // }, [])
 
   return(
-    <div>
-      <h1>CraftNest Marketplace</h1>
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginSignUp />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <div className="message">
+        This page is currently under construction 🥱
+    </div>
+    
+  )
+}
+
+export default HomePage
